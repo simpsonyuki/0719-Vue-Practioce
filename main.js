c4149e427c728a8f18343be5e38534be3ea9a5fe
simpsonyuki <yukilin0327@gmail.com>
2020-07-19 11:53:27 +0800

    定義渲染區域

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("I'm JS~");
+
+// 定義渲染區域
+// Vue和JS、JQuery最大的不同在於：Vue可以指定作用於特定元素，其他兩者皆為搜尋整個Document文件
+// 寫法: var 變數名稱 = new Vue(); → 表示: 我要新增一個 Vue 物件
+var vue1 = new Vue({
+    el : '#vue1',     // el : 'CSS選取器' → 指定元素，使用雙引號或單引號都可以(el是element的縮寫)
+    
+
+})
